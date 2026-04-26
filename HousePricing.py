85fadf85b186ecd2232ee9652388c65a8de3dcbc
Issa Hasan <issahsn36@gmail.com>
2026-04-26 07:24:45 +0300

    Refactor HousePricing.py and add comments
    Refactored code for clarity and added comments for better understanding.

--- HousePricing.py
+++ HousePricing.py
@@ -6,37 +6,50 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import os
 import seaborn as sns
-df=pd.read_csv("cleaned_data.csv") # if it does not work you can put the full path  
+
+#Load the data 
+df=pd.read_csv("cleaned_data.csv") # if it does not work you can put the full path 
+
+#Selecting the features and target column 
 x=df.drop('SalePrice', axis=1)  
-y=df['SalePrice']  
+y=df['SalePrice'] 
+
+# splitting the data into training and testing sets (80, 20)
 x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=42)
 
+# 3. initializing the XGBoost regressor with GPU support
 model = xgb.XGBRegressor(
-    n_estimators=1500,
+    n_estimators=1500,#trees in the forest
     learning_rate=0.05,
     max_depth=5,    
-    tree_method='hist',
-    device='cuda',           
+    tree_method='hist',# algorithm for building trees (histogram-based, efficient for large datasets)
+    device='cuda',           # use GPU for training    
 )
+# 4. you maybe hear a fan noise, it's the GPU working hard! 
 
+# training the model (this may take a few seconds to a couple of minutes depending on your GPU)
 print("Training started... 🌪️")
 model.fit(x_train, y_train,verbose=False,eval_set=[(x_train,y_train)])
 print("Training finished successfully!")
 
+# 5. testing the model (making predictions)
 predictions = model.predict(x_test)
 
+# 6. Evaluating the model's performance
 rmse = np.sqrt(mean_squared_error(y_test, predictions))
 r2 = r2_score(y_test, predictions)
 os.system("cls")
 print("-" * 30)
 print(f"✅ RMSE (Root Mean Squared Error): ${rmse:,.2f}")
 print(f"🎯 R-squared (Accuracy): {r2 * 100:.2f}%")
+
+# 1. sorting features by importance (gain) and selecting the top 10
 importances = model.feature_importances_
 feature_names = x.columns
-
 feature_importance_df = pd.DataFrame({'Feature': feature_names, 'Importance': importances})
 feature_importance_df = feature_importance_df.sort_values(by='Importance', ascending=False).head(10)
 
+# 3. visualizing the top 10 features influencing house prices
 plt.figure(figsize=(12, 6))
 sns.barplot(x='Importance', y='Feature', data=feature_importance_df, palette='magma')
 plt.title('Top 10 Features Influencing House Prices')
